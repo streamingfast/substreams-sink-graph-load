fa82b28c8256bddc6a71ddc7a15a8f3caa5016f9
colindickson <colin.dickson@gmail.com>
2023-08-05 12:30:40 -0400

    skip boolean nil check

--- csvprocessor/entity.go
+++ csvprocessor/entity.go
@@ -47,6 +47,10 @@ func (e *Entity) ValidateFields(desc *schema.EntityDesc) error {
 				return nil
 			}
 
+			if field.Type == "Boolean" {
+				return nil
+			}
+
 			return fmt.Errorf("field %q cannot be nil on entity %s at ID %s", field.Name, field.Type, e.Fields["id"])
 		}
 	}
